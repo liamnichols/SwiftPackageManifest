00258c4040a28d749364471c1f82d33b6e305488
Liam Nichols <liam.nichols.ln@gmail.com>
2022-01-08 01:35:28 +0100

    Add DeviceFamilyCondition helper getter to Capability

--- Sources/ManifestDescription/IOSAppInfo+Util/Capability+Util.swift
+++ Sources/ManifestDescription/IOSAppInfo+Util/Capability+Util.swift
@@ -62,4 +62,28 @@ public extension ProductSetting.IOSAppInfo.Capability {
             return .userTracking
         }
     }
+
+    var condition: ProductSetting.IOSAppInfo.DeviceFamilyCondition? {
+        switch self {
+        case .appTransportSecurity(_, let condition),
+                .bluetoothAlways(_, let condition),
+                .calendars(_, let condition),
+                .camera(_, let condition),
+                .contacts(_, let condition),
+                .faceID(_, let condition),
+                .localNetwork(_, _, let condition),
+                .locationAlwaysAndWhenInUse(_, let condition),
+                .locationWhenInUse(_, let condition),
+                .mediaLibrary(_, let condition),
+                .microphone(_, let condition),
+                .motion(_, let condition),
+                .nearbyInteractionAllowOnce(_, let condition),
+                .photoLibrary(_, let condition),
+                .photoLibraryAdd(_, let condition),
+                .reminders(_, let condition),
+                .speechRecognition(_, let condition),
+                .userTracking(_, let condition):
+            return condition
+        }
+    }
 }
